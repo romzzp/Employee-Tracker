88a35a241bc335fa090ce303a630430e2772d1c9
romzz.git <romzz.patel@gmail.com>
2021-11-19 11:02:20 -0500

    added function to get employee data

--- server.js
+++ server.js
@@ -84,3 +84,20 @@ function employeeSearch() {
             }
         });
 }
+
+function employeeData() {
+    var query = `SELECT e.id, e.first_name, e.last_name, r.title, d.name AS department, r.salary, CONCAT(m.first_name, ' ', m.last_name) AS manager FROM employee e
+    LEFT JOIN role r
+      ON e.role_id = r.id
+    LEFT JOIN department d
+    ON d.id = r.department_id
+    LEFT JOIN employee m
+      ON m.id = e.manager_id`;
+    connection.query(query, function (err, res) {
+        if (err) throw err;
+        // console.log("Error trying to get all employees");
+        console.table(res);
+        console.log("All employees were viewed.");
+        employeeSearch();
+    });
+}
