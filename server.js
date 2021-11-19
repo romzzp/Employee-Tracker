e762e647ef19d740f1f8dcc7d2830fee8bb51c1c
romzz.git <romzz.patel@gmail.com>
2021-11-19 11:32:47 -0500

    added function to get employee's pay

--- server.js
+++ server.js
@@ -134,3 +134,20 @@ function empRole() {
         employeeSearch();
     });
 }
+
+function empPay() {
+    var query = `SELECT CONCAT(e.first_name, ' ', e.last_name) AS EMPLOYEE, r.salary AS SALARY
+    FROM employee e
+      LEFT JOIN role r
+        ON e.role_id = r.id
+      LEFT JOIN department d
+      ON d.id = r.department_id
+      LEFT JOIN employee m
+        ON m.id = e.manager_id`;
+    connection.query(query, function (err, res) {
+        if (err) throw err;
+        console.table(res);
+        console.log("All Employees' salary were viewed.");
+        employeeSearch();
+    });
+}
